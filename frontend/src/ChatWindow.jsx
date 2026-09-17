607c95f4d5d761d51f440ba30a1341747916e6f4
Ramkrishna Upadhyay <goluupadhyay600@gmail.com>
2026-09-17 23:03:52 +0530

    Fix: updated deprecated Groq models and local API URL

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -33,7 +33,7 @@ if (token) {
     }
 
     try{
-      const response= await fetch("http://localhost:8080/api/chat",options);//https://boltgpt.onrender.com/api/chat
+      const response= await fetch("https://boltgpt.onrender.com/api/chat",options); //http://localhost:8080/api/chat
        const res=  await response.json();
       console.log(res);
       setReply(res.reply);
